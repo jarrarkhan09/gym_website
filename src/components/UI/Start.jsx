7cfa51cc0640c3e7f5e0304054266a2c67335104
Jarrar khan <mjk201009@gmail.com>
2024-08-27 23:30:46 +0530

    update gym

--- src/components/UI/Start.jsx
+++ src/components/UI/Start.jsx
@@ -1,15 +1,29 @@
-import React from 'react';
-import '../../styles/start.css';
+import React from "react";
+import "../../styles/start.css";
 import trainerImg from "../../assets/img/trainer.png";
-
 
 const Start = () => {
   return (
     <section>
-      
+      <div className="container">
+        <div className="start__wrapper">
+          <div className="start__img">
+            <img src={trainerImg} alt="" />
+          </div>
+          <div className="start__content">
+            <h2 className="section__title">Ready to make a <span className="highlights">change?</span></h2>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia
+              voluptatum repellendus, molestiae deleniti quia, nesciunt ex
+              corrupti error aliquam vero quam possimus dolore neque nemo saepe
+              magni ratione! Hic, voluptas.
+            </p>
+            <button className="register__btn">Get Started</button>
+          </div>
+        </div>
+      </div>
     </section>
-
-  )
-}
+  );
+};
 
 export default Start;
